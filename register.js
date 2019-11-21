fd3f0633876894c13875f34bc3e09aee16389c04
Fabian Sundholm <fabian.sundholm@hotmail.com>
2019-11-21 15:07:47 +0100

    attempt to do register error handling and fail

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 var registerUser = firebase.functions().httpsCallable('registerUser');
 
-function registerUser() {
+function localRegisterUser() {
   var username = document.getElementById('username')
   var email = document.getElementById('email')
   var password = document.getElementById('password')
@@ -8,7 +8,12 @@ function registerUser() {
     email: email.value,
     password: password.value,
     displayName: username.value
-  })
+  }).then(function(stuff) {
+    console.log(stuff);
+  }).catch(function(error) {
+    console.log(error);
+    console.log("hej");
+  });
 }
 
 document.getElementById('register-button').addEventListener("click", function(event) {
